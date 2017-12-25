e1a52837f1f6a183476711df6979b8fe35a144e9
ksnell19 <ksnell@Kristins-MacBook-Pro.local>
2017-12-24 18:37:56 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//create button
+// var button= document.createElement('button');
+// button.textContent= "Submit";
+// button.setAttribute('onclick', 'getInfo()');
+// document.body.appendChild(button);
+
+//create variables
+var userInfo = document.getElementById('username');
+var commentInfo= document.getElementById('comment');
+
+//create function for the button
+function getInfo(){
+  var newDiv= document.createElement('div');
+  var newH5 = document.createElement('h5');
+  var newP= document.createElement('p');
+
+  newDiv.appendChild(newH5);
+  newDiv.appendChild(newP);
+  newH5.innerText= "Username: " + userInfo.value;
+  newP.innerText= "Comment: " + commentInfo.value;
+
+  document.body.appendChild(newDiv);
+};
